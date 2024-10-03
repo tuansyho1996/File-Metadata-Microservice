9a6cfdaf1ca6f351bab29f21da4031f9a9e8b80a
tuansyho1996 <hosytuan101296@gmail.com>
2024-10-03 23:50:24 +0700

    feat: update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -10,7 +12,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
 
 
 
